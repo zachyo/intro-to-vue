135269fc5714e41bf02a8cdd772d47ec221f8d27
zacheusIsrael <zacheazy99@yahoo.com>
2023-01-05 07:21:05 +0100

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// import Vue from 'vue';
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// Vue.config.productionTip = false;
+//global directive
+// Vue.directive('focused', {
+//     inserted : function(el) {
+//         el.focus();
+//     }
+// });
+createApp(App).mount('#app')
