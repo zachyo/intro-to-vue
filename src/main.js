02a01967e00a1f640c799e39cc5dceec618bfd7b
zacheusIsrael <segunzacheusi@gmail.com>
2023-02-16 08:45:15 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,31 @@
 // import Vue from 'vue';
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "./components/HelloWorld.vue";
+import DogJokes from "./components/DogJokes.vue";
+// import {store} from './store/index.js';
 
+// Vue.config.productionTip = false;
+// 
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+      props: {
+        name: "ZarKie",
+      }
+    },
+    {
+      path: "/dogjokes",
+      name: "dogjokes",
+      component: DogJokes,
+    },
+  ],
+});
 // Vue.config.productionTip = false;
 //global directive
 // Vue.directive('focused', {
@@ -9,4 +33,5 @@ import App from './App.vue'
 //         el.focus();
 //     }
 // });
-createApp(App).mount('#app')
+// new Vue({store, render: (h) => h(App)}).$mount("#app");
+createApp(App).use(router).mount("#app");
